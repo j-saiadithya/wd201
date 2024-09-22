7fe6d12348a91ef70aff22db9359494baaae29fb
j-saiadithya <jsaiadithya1020@gmail.com>
2024-09-22 08:54:10 +0530

    updated code acc to test cases

--- http-server/index.js
+++ http-server/index.js
@@ -1,47 +1,51 @@
-// const http=require("http");
-// const fs=require("fs");
-// const server=http.createServer((req,res)=>{
-//     const stream=fs.createReadStream("sample.txt");
-//     stream.pipe(res);
-//     // fs.readFile("sample.txt",(err,data)=>{
-//     //     res.end(data);
-//     // })
-// });
-// server.listen(3000);
-
-
 const http = require("http");
 const fs = require("fs");
+const minimist = require("minimist");
+
+// Parse command-line arguments
+const argv = minimist(process.argv.slice(2));
+const port = argv.port || 3000; // Default to port 3000 if not provided
 
 let homeContent = "";
 let projectContent = "";
+let registrationContent = "";
 
+// Read HTML files asynchronously
 fs.readFile("home.html", (err, home) => {
   if (err) {
     throw err;
   }
   homeContent = home;
-});
 
-fs.readFile("project.html", (err, project) => {
-  if (err) {
-    throw err;
-  }
-  projectContent = project;
-});
-http
-  .createServer((request, response) => {
-    let url = request.url;
-    response.writeHeader(200, { "Content-Type": "text/html" });
-    switch (url) {
-      case "/project":
-        response.write(projectContent);
-        response.end();
-        break;
-      default:
-        response.write(homeContent);
-        response.end();
-        break;
+  fs.readFile("project.html", (err, project) => {
+    if (err) {
+      throw err;
     }
-  })
-  .listen(3000);
+    projectContent = project;
+
+    fs.readFile("registration.html", (err, registration) => {
+      if (err) {
+        throw err;
+      }
+      registrationContent = registration;
+
+      http.createServer((request, response) => {
+        let url = request.url;
+
+        response.writeHead(200, { "Content-Type": "text/html" });
+        switch (url) {
+          case "/project":
+            response.write(projectContent);
+            break;
+          case "/registration":
+            response.write(registrationContent);
+            break;
+          default:
+            response.write(homeContent);
+            break;
+        }
+        response.end();
+      }).listen(port); // Use the specified port
+    });
+  });
+});
